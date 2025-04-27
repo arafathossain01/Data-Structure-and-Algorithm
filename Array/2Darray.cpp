8d23bd94b90ec9175a0ce628fb2116f66c7fd2e8
Arafat <arafathossain.cs@gmail.com>
2025-04-27 22:36:43 +0600

    add 2Darray

--- Array/2Darray.cpp
+++ Array/2Darray.cpp
@@ -3,17 +3,19 @@ using namespace std;
 
 int main()
 {
-    int Array[3][4] = {{1, 2, 3, 4}, 
-    {5, 6, 7, 8}, 
-    {9, 10, 11, 12}};
+    int Array[3][4] = {{1, 2, 3, 4},
+                       {5, 6, 7, 8},
+                       {9, 10, 11, 12}};
 
-    cout<<"Array is: "<<endl;
+    cout << "Array is: " << endl;
 
-    for(int i=0; i<3; i++){
-        for(int j=0; j<4; j++){
-            cout<< Array[i][j]<<" ";
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << Array[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
