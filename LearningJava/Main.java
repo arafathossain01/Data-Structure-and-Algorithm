b35aad231038dc48a785b299af033198db3b7fbf
Arafat <arafathossain.cs@gmail.com>
2025-09-09 01:08:50 +0600

    addad LearningJava folder

---
+++ LearningJava/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+/*  
+Java সব কোড একটা class এর ভিতরে থাকে। Class হলো একটা design বা blueprit যেখানে method , variable, data কিভাবে সাজান থাকবে তা নির্ধারণ করা হয়। java তে file এর নাম যা হবে class এর নামও তাই হবে। 
+ 
+উপরের কোডে Main.java file এর নাম অনুযায়ী class এর নাম রাখা হয়েছে এবং Main class এর ভিতরে method লিখা হয়েছে। প্রত্যেক java program এর ভিতর main method থাকা must.
+main method থেকে java code run করা শুরু করে। 
+
+System হলো java এর একটা built-in java class যা অনেক memeber contain করে যেমন out. out হলো output এর short form. পুরাটা System.out.println দিয়ে Java code print করা হয়। 
+
+public class মানে এটাকে যেকোনো জায়গা থেকে access করা যাবে। publci static void main(String[] args) এটা হলো main method.
+*/
