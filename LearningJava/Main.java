136c67eed4b1c1210de6998908bb8ca9f3431533
Arafat <arafathossain.cs@gmail.com>
2025-09-09 09:18:40 +0600

    update Learning java

--- LearningJava/Main.java
+++ LearningJava/Main.java
@@ -5,7 +5,7 @@ public class Main{
 }
 
 /*  
-Java সব কোড একটা class এর ভিতরে থাকে। Class হলো একটা design বা blueprit যেখানে method , variable, data কিভাবে সাজান থাকবে তা নির্ধারণ করা হয়। java তে file এর নাম যা হবে class এর নামও তাই হবে। 
+Java সব কোড একটা class এর ভিতরে থাকে। Class হলো একটা design বা blueprit যেখানে method , variable, data কিভাবে সাজান থাকবে তা নির্ধারণ করা হয়। java তে file এর নাম যা হবে class এর নামও তাই হবে। public class শুধুমাত্র file এর নাম অনুজায়ে হবে। অন্য class name শুধু class name হবে কোনো public add করতে পারবো নাহ। 
  
 উপরের কোডে Main.java file এর নাম অনুযায়ী class এর নাম রাখা হয়েছে এবং Main class এর ভিতরে method লিখা হয়েছে। প্রত্যেক java program এর ভিতর main method থাকা must.
 main method থেকে java code run করা শুরু করে। 
@@ -13,4 +13,12 @@ main method থেকে java code run করা শুরু করে।
 System হলো java এর একটা built-in java class যা অনেক memeber contain করে যেমন out. out হলো output এর short form. পুরাটা System.out.println দিয়ে Java code print করা হয়। 
 
 public class মানে এটাকে যেকোনো জায়গা থেকে access করা যাবে। publci static void main(String[] args) এটা হলো main method.
+
+public → মানে এই method সবাই ব্যবহার করতে পারবে।
+
+static → মানে object বানানো ছাড়া এটা চালানো যাবে।
+
+void → মানে কিছু return করবে না।
+
+String[] args → command line থেকে ডাটা নেওয়ার জন্য ব্যবহার করা হয়।
 */
