9142702b45a8333e5d3530c6de1364f977db36d1
Arafat <arafathossain.cs@gmail.com>
2025-09-25 20:49:55 +0600

    update

--- LearningJava/FirstLecture/src/Main.java
+++ LearningJava/FirstLecture/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 
 /*  
-Java সব কোড একটা class এর ভিতরে থাকে। Class হলো একটা design বা blueprit যেখানে method , variable, data কিভাবে সাজান থাকবে তা নির্ধারণ করা হয়। java তে file এর নাম যা হবে class এর নামও তাই হবে। public class শুধুমাত্র file এর নাম অনুজায়ে হবে। অন্য class name শুধু class name হবে কোনো public add করতে পারবো নাহ। 
+Java সব কোড একটা class এর ভিতরে থাকে। Class হলো একটা design বা blueprit যেখানে method , variable, data কিভাবে সাজানো থাকবে তা নির্ধারণ করা হয়। java তে file এর নাম যা হবে class এর নামও তাই হবে। public class শুধুমাত্র file এর নাম অনুযায়ে হবে। অন্য class name শুধু class name হবে কোনো public add করতে পারবো নাহ। 
  
 উপরের কোডে Main.java file এর নাম অনুযায়ী class এর নাম রাখা হয়েছে এবং Main class এর ভিতরে method লিখা হয়েছে। প্রত্যেক java program এর ভিতর main method থাকা must.
 main method থেকে java code run করা শুরু করে। 
