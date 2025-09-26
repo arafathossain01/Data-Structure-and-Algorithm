9d99a8505eb8b49817baeda78d0cb458a34866e9
Arafat <arafathossain.cs@gmail.com>
2025-09-26 20:43:15 +0600

    Adding.java

---
+++ LearningJava/FirstLecture/src/SecondPart/Adding.java
@@ -0,0 +1,19 @@
+package SecondPart;
+
+import java.util.Scanner;
+
+public class Adding {
+    public static void main(String[] args) {
+
+        System.out.print("Enter two numbers: ");
+        Scanner input = new Scanner(System.in);
+
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum is: " + sum);
+        input.close();
+    }
+}
