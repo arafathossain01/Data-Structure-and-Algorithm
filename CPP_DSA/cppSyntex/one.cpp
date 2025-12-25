105c80e67fec056b294751e986ecbc82bc1274e7
Arafat Hossain <arafathossain.cs@gmail.com>
2025-12-25 19:08:32 +0600

    cpp updated

---
+++ CPP_DSA/cppSyntex/one.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    cout << "This is a " << "\n"
+         << "basic c++ syntex";
+    return 0;
+}
---
+++ CPP_DSA/cppSyntex/two.cpp
@@ -0,0 +1,16 @@
+// input and output
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int a = 20;
+    int b = 40;
+    string x = "Hello Programmer";
+
+    cout << a << "+" << b << " = " << a + b << "\n"
+         << x;
+    return 0;
+}
